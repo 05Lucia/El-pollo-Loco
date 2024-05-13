bac8f6705f40dea1b9f57605a7b1da1cde55d21f
Alice Buchholz <alice.buchholz@gmail.com>
2024-05-13 10:40:18 +0200

    Update chicken.class.js

--- classes/chicken.class.js
+++ classes/chicken.class.js
@@ -2,6 +2,8 @@ class Chicken extends MovebaleObject{
 
     constructor() {
         super().loadeImage('./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+
+        this.x = 300+Math.random()*500;
     }
     
 }
