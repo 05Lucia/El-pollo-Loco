ad24463dfa83f0bd1f3f5cd20cb43a549cc9ad1f
Alice Buchholz <alice.buchholz@gmail.com>
2024-05-14 09:42:08 +0200

    add cloud

---
+++ classes/cloud.class.js
@@ -0,0 +1,14 @@
+class Cloude extends MovebaleObject{
+y = 20;
+height = 300;
+width = 500;
+
+    constructor() {
+        super().loadeImage('./img/5_background/layers/4_clouds/1.png');
+
+        this.x = Math.random()*400;
+        
+        
+        
+    }
+}
