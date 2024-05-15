7b8573f12a417ace49ea258fa187fc145b93752d
Alice Buchholz <alice.buchholz@gmail.com>
2024-05-15 10:58:39 +0200

    img Cache

--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -4,16 +4,26 @@ class MovebaleObject {
     img;
     height = 150;
     width = 100;
+    imageCache = {};
 
     loadeImage(path) {
         this.img = new Image(); // this.img = document.getElementById('image') <img id="image"
         this.img.src = path;
     }
 
+    loadeImages(array) {
+        array.forEach(path => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = path;
+        });
+
+    }
+
     moveRight() {
         console.log('Moving right')
     }
-    
+
     moveLight() {
 
     }
